e3ed796dc3c8fa45fb459d36c718e4727a439a4e
Gary Meehan <wingstand@googlemail.com>
2016-06-21 15:00:02 +0100

    Initial commit of Windows Phone code

---
+++ src/wp8/MediaPropertiesPlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+
+using System.Diagnostics;
+
+using Newtonsoft.Json.Linq;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+  public sealed class MediaPropertiesPlugin : BaseCommand
+  {
+    public MediaPropertiesPlugin()
+      : base()
+    {
+    }
+
+    public void getProperties(string args)
+    {
+      string callbackId = "";
+      string path = "";
+
+      try
+      {
+        JArray parameters = JArray.Parse(args);
+
+        path = (string) parameters[0];
+        callbackId = (string) parameters[1];
+
+        GetProperties(path, callbackId);
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine("Cannot get properties for {0}: {1}", path, e.Message);
+
+        DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, e.Message), callbackId);
+      }
+    }
+
+    private async void GetProperties(string path, string callbackId)
+    {
+      try
+      {
+        StorageFolder rootFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+        StorageFile file = await rootFolder.GetFileAsync(path);
+        JObject result = new JObject();
+
+        result["contentType"] = file.ContentType;
+
+        if (file.ContentType.StartsWith("audio"))
+        {
+          MusicProperties properties = await file.Properties.GetMusicPropertiesAsync();
+
+          result["duration"] = properties.Duration.TotalSeconds;
+        }
+        else if (file.ContentType.StartsWith("video"))
+        {
+          VideoProperties properties = await file.Properties.GetVideoPropertiesAsync();
+
+          result["duration"] = properties.Duration.TotalSeconds;
+        }
+
+        DispatchCommandResult(new PluginResult(PluginResult.Status.OK, result.ToString()), callbackId);
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine("Cannot get properties for {0}: {1}", path, e.Message);
+
+        DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, e.Message), callbackId);
+      }
+    }
+  }
+}
---
+++ www/media-properties.js
@@ -0,0 +1,7 @@
+﻿var argscheck = require('cordova/argscheck'), exec = require('cordova/exec');
+
+module.exports = {
+  getProperties: function (successCallback, errorCallback, path) {
+    cordova.exec(successCallback, errorCallback, "MediaProperties", "getProperties", [path]);
+  }
+}
